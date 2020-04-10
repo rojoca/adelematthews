16f3dfa4c1248bc83cb52d2220ed41815949bda6
Rory Casey <rory@sc.nz>
2020-04-11 01:28:52 +1200

    Make env variables lowercase

--- pages/api.js
+++ pages/api.js
@@ -2,14 +2,14 @@ import fetch from "node-fetch";
 
 export async function getItems() {
   const response = await fetch(
-    `https://${process.env.CLOUDINARY_KEY}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/tags/adelematthews?context=true`
+    `https://${process.env.cloudinary_key}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/tags/adelematthews?context=true`
   );
   return response.json();
 }
 
 export async function getItem(publicId) {
   const response = await fetch(
-    `https://${process.env.CLOUDINARY_KEY}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/upload/${publicId}?context=true`
+    `https://${process.env.cloudinary_key}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/upload/${publicId}?context=true`
   );
   return response.json();
 }
