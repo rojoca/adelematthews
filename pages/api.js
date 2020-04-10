7f997eceeb801f9b41f1f445ab7ee72a126ac5e5
Rory Casey <rory@sc.nz>
2020-04-11 01:34:06 +1200

    Add now.json for env vars

--- pages/api.js
+++ pages/api.js
@@ -1,18 +1,16 @@
 import fetch from "node-fetch";
 
 export async function getItems() {
-  console.log(process.env.cloudinary_key);
-  console.log(process.env.CLOUDINARY_KEY);
-  console.log(process.env);
+  console.log(process.env.CLOUDINARY_KEY, process.env.CLOUDINARY_SECRET);
   const response = await fetch(
-    `https://${process.env.cloudinary_key}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/tags/adelematthews?context=true`
+    `https://${process.env.CLOUDINARY_KEY}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/tags/adelematthews?context=true`
   );
   return response.json();
 }
 
 export async function getItem(publicId) {
   const response = await fetch(
-    `https://${process.env.cloudinary_key}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/upload/${publicId}?context=true`
+    `https://${process.env.CLOUDINARY_KEY}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/upload/${publicId}?context=true`
   );
   return response.json();
 }
