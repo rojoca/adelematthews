7160bb47dde769f38aed73870011bf7867d266f5
Rory Casey <rory@sc.nz>
2020-04-11 01:46:47 +1200

    Remove debug output

--- pages/api.js
+++ pages/api.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 
 export async function getItems() {
-  console.log(process.env.CLOUDINARY_KEY, process.env.CLOUDINARY_SECRET);
   const response = await fetch(
     `https://${process.env.CLOUDINARY_KEY}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/tags/adelematthews?context=true`
   );
