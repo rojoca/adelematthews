afbf7a051a5f9e967d44e5d1f644c6f3d8dffe69
Rory Casey <rory@sc.nz>
2020-04-11 01:30:44 +1200

    Add more debug

--- pages/api.js
+++ pages/api.js
@@ -1,6 +1,9 @@
 import fetch from "node-fetch";
 
 export async function getItems() {
+  console.log(process.env.cloudinary_key);
+  console.log(process.env.CLOUDINARY_KEY);
+  console.log(process.env);
   const response = await fetch(
     `https://${process.env.cloudinary_key}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/rojoca/resources/image/tags/adelematthews?context=true`
   );
